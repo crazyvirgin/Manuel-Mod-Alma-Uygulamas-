0fb285a18b53958cd5e4ab3fdc13b6651ff18295
crazyvirgin <41253964+crazyvirgin@users.noreply.github.com>
2023-08-15 20:46:44 +0300

    Proje dosyası ekle.

---
+++ Manuel Mod Alma Uygulaması/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manuel_Mod_Alma_Uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int Sayı = 0;
+                int Bölen = 0;
+                int Bölüm = 0;
+                int Kalan = 0;
+
+                Console.WriteLine("Mod Alma Uygulanması");
+                Console.Write("Modu Alınacak Sayıyı Giriniz =");
+                Sayı = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Modu Giriniz =");
+                Bölen=Convert.ToInt32((Console.ReadLine()));
+                Bölüm = Sayı / Bölen;
+                Kalan = Sayı-(Bölüm * Bölen);
+                Console.WriteLine("{0} in {1} Bölümünün sonucu {2} Kalan ise(MOD) {3}", Sayı, Bölen, Bölüm, Kalan);
+
+
+            }
+
+        }
+    }
+}
---
+++ Manuel Mod Alma Uygulaması/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Manuel Mod Alma Uygulaması")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Manuel Mod Alma Uygulaması")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("20697a6f-99f7-4a87-a10b-72eb74e58294")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
